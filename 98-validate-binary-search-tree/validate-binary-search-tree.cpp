cd44823f02fa3c3bc7622c1776cf6baae4ee94ef
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-04 11:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (46.85%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,46 +11,61 @@
  */
 class Solution {
 public:
+
+    bool helper(TreeNode* root,long lowbound,long upbound){
+        if(root==NULL)
+            return true;
+        if( root->val>=upbound || root->val<=lowbound)
+            return false;
+        return helper(root->left,lowbound,root->val) && 
+        helper(root->right,root->val,upbound);
+    }
+
     bool isValidBST(TreeNode* root) {
-        // to check BST,we have to check every  node
-                                    // O(n)- min TC
+                        // to check BST,we have to check every  node
         
-        // Inorder of BST= Sorted
-        // Morris traversal = check inorder
+                                    // Inorder of BST= Sorted
+                                    // Morris traversal = check inorder
 
-        TreeNode* curr=root;
-        TreeNode* prev=NULL;
-        bool ans=true;
+                                    // O(n)- min TC , O(1)
+        // TreeNode* curr=root;
+        // TreeNode* prev=NULL;
+        // bool ans=true;
 
-        while(curr){
-            if(curr->left){
-                TreeNode* leftnode=curr->left;
+        // while(curr){
+        //     if(curr->left){
+        //         TreeNode* leftnode=curr->left;
 
-                while(leftnode->right && leftnode->right!=curr)
-                    leftnode=leftnode->right;
+        //         while(leftnode->right && leftnode->right!=curr)
+        //             leftnode=leftnode->right;
 
-                if(leftnode->right==NULL){
-                    leftnode->right=curr;
-                    curr=curr->left;
-                }
+        //         if(leftnode->right==NULL){
+        //             leftnode->right=curr;
+        //             curr=curr->left;
+        //         }
 
-                else{    // leftnode->right=curr
-                    leftnode->right=NULL;
-                    if(prev && prev->val>=curr->val)
-                        ans= false;
-                    prev=curr;
-                    curr=curr->right;
-                }
-            }
-            else{
-                if(prev && prev->val>=curr->val)
-                    ans= false;
-                prev=curr;
-                curr=curr->right;
-            }
+        //         else{    // leftnode->right=curr
+        //             leftnode->right=NULL;
+        //             if(prev && prev->val>=curr->val)
+        //                 ans= false;
+        //             prev=curr;
+        //             curr=curr->right;
+        //         }
+        //     }
+        //     else{
+        //         if(prev && prev->val>=curr->val)
+        //             ans= false;
+        //         prev=curr;
+        //         curr=curr->right;
+        //     }
 
 
-        }
-        return ans;
+        // }
+        // return ans;
+
+
+                            // recursive code O(n),O(n)
+
+        return helper(root,LONG_MIN,LONG_MAX);
     }
 };
