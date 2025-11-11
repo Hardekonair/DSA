071d73457c90f3b3704960b724fce16c2085cf4f
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-11 11:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.80%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     int dp[100];
-    int helper(int i,vector<int>& nums){
-        if(i==0)
-            return nums[i];
-        if(i<0)
-            return 0;
+                            // MEMOIZATION
+    // int helper(int i,vector<int>& nums){
+    //     if(i==0)
+    //         return nums[i];
+    //     if(i<0)
+    //         return 0;
 
-        if(dp[i]!=-1)
-            return dp[i];
+    //     if(dp[i]!=-1)
+    //         return dp[i];
 
-        int pick=nums[i]+helper(i-2,nums);
-        int notpick=0+helper(i-1,nums);
+    //     int pick=nums[i]+helper(i-2,nums);
+    //     int notpick=0+helper(i-1,nums);
 
-        return dp[i]=max(pick,notpick);
+    //     return dp[i]=max(pick,notpick);
+    // }
+
+                                // TABULATION
+    int helper2(vector<int>& nums){
+        if(nums.size()==1)
+            return nums[0];
+        int ans=INT_MAX;
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            int pick=nums[i]+dp[i-2];
+            int notpick=0+dp[i-1];
+            dp[i]=max(pick,notpick);
+        }
+
+        return dp[nums.size()-1];
     }
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return helper(nums.size()-1,nums);
+        // return helper(nums.size()-1,nums);
+
+        return helper2(nums);
         
     }
 };
