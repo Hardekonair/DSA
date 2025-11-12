fe09911824060326172d307d14b158595f57919b
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-12 15:41:02 +0530

    Time: 6 ms (6.06%) | Memory: 16.5 MB (13.68%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,62 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int helper1(vector<vector<int>>& grid,int m,int n){
-        if(m==0 && n==0)
+    vector<vector<int>> dp;
+    // RECURSION
+    int helper1(vector<vector<int>>& grid, int m, int n) {
+        if (m == 0 && n == 0)
             return grid[m][n];
-        
-        if(m<0 || n<0)
+
+        if (m < 0 || n < 0)
             return INT_MAX;
 
-        if(dp[m][n]!=-1)
+        int right = helper1(grid, m, n - 1);
+        int down = helper1(grid, m - 1, n);
+
+        return grid[m][n] + min(right, down);
+    }
+
+    // MEMOIZATION
+    int helper2(vector<vector<int>>& grid, int m, int n) {
+        // if(m==0 && n==0)
+        //     return grid[m][n];
+        dp[0][0] = grid[0][0];
+
+        if (m < 0 || n < 0)
+            return INT_MAX;
+
+        if (dp[m][n] != -1)
             return dp[m][n];
 
-        int right=helper1(grid,m,n-1);
-        int down=helper1(grid,m-1,n);
+        int right = helper1(grid, m, n - 1);
+        int down = helper1(grid, m - 1, n);
 
-        return dp[m][n]=grid[m][n]+min(right,down);
+        return dp[m][n] = grid[m][n] + min(right, down);
+    }
+
+    // TABULATION
+    int helper3(vector<vector<int>>& grid, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    dp[0][0] = grid[0][0];
+                else {
+                    dp[i][j] = grid[i][j];
+                    int right=INT_MAX,up=INT_MAX;
+                    if(i-1>=0)
+                        right=dp[i-1][j];
+                    if(j-1>=0)
+                        up=dp[i][j-1];
+                    dp[i][j]+=min(right,up);
+                    
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        dp.resize(m,vector<int>(n,-1));
-        return helper1(grid,m-1,n-1);
+        int m = grid.size();
+        int n = grid[0].size();
+        dp.resize(m, vector<int>(n, -1));
+        return helper3(grid, m , n );
     }
 };
