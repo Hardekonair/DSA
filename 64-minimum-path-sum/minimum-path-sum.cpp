1f0b25f07bb277c3843e9e32965774e25534e062
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-12 16:07:21 +0530

    Time: 3 ms (31.05%) | Memory: 17.1 MB (13.68%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -53,10 +53,28 @@ public:
         }
         return dp[m - 1][n - 1];
     }
+
+    //SPACE OPTIMIZED
+    int helper4(vector<vector<int>> grid,int m,int n){
+
+        for(int i=1;i<m;i++){
+            grid[i][0]+=grid[i-1][0];
+        }
+        for(int i=1;i<n;i++){
+            grid[0][i]+=grid[0][i-1];
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                grid[i][j]=grid[i][j]+min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         dp.resize(m+1, vector<int>(n, -1));
-        return helper3(grid, m , n );
+        return helper4(grid, m , n );
     }
 };
