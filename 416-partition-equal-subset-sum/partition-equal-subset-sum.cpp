60d34af9b4f5cfc839e01af7197463bcb55d0720
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-15 12:45:20 +0530

    Time: 180 ms (18.09%) | Memory: 204.2 MB (7.67%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -16,6 +16,21 @@ public:
 
         return dp[i][s2]=(pick | notpick);
     }
+    bool equalsubset1(vector<int>& arr, int i, int s1,int s2){
+        if(s2==s1)
+            return true;
+        if(i<0)
+            return false;
+        if(dp[i][s1]!=-1)
+            return dp[i][s1];
+        
+        bool pick=false,notpick=false;
+        if(s2<=s1)
+            pick=equalsubset1(arr,i-1,s1-arr[i],s2+arr[i]);
+        notpick=equalsubset1(arr,i-1,s1,s2);
+
+        return dp[i][s1]=(pick | notpick);
+    }
     bool canPartition(vector<int>& nums) {
         int s1=0,s2=0;
         for(auto it:nums)
@@ -23,6 +38,6 @@ public:
         if(s1%2!=0)
             return false;
         dp.resize(nums.size(),vector<int>(s1+1,-1));
-        return equalsubset(nums,nums.size()-1,s1,s2);
+        return equalsubset1(nums,nums.size()-1,s1,s2);
     }
 };
