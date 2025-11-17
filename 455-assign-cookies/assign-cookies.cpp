ede647b00de5bef5f5b7df6fb87fb2c29814f9a0
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-17 11:07:58 +0530

    Time: 7 ms (73.15%) | Memory: 44.7 MB (95.62%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // int helper(vector<int>& g, vector<int>& s,int i,int j){
+    //     if(j<0 || i<0){
+    //         // if(c==g.size())
+    //             return 1;
+    //     }
+        
+    //     int notpick=helper(g,s,i-1,j);
+    //     int pick=0;
+    //     if(g[i]>=s[i]){
+    //         pick=helper(g,s,i-1,j-1);
+    //         // c++;
+    //     }
+
+    //     return max(pick);        
+    // }
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int c=0;
+        int i=g.size()-1,j=s.size()-1;
+        while(i>=0 && j>=0){
+            if(g[i]<=s[j]){
+                c++;
+                i--;
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+        return c;
+    }
+};
