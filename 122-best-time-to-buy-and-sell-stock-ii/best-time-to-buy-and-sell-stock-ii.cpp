70872ce020be5bc5711b150cf5d7d50dd4cfec5d
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-21 14:26:29 +0530

    Time: 11 ms (5.89%) | Memory: 21.7 MB (16.48%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,8 +17,32 @@ public:
         }
         return dp[i][buy]=profit;
     }
+
+    int maxp1(vector<int>& prices){
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        // if(i==prices.size())
+        //     return 0;
+        dp[prices.size()][0]=0;
+        dp[prices.size()][1]=0;
+
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                int profit=0;
+                if(j){
+                    profit=max(-prices[i]+dp[i+1][0],0+dp[i+1][1]);
+                
+                }
+                else if(!j){
+                    profit=max(prices[i]+dp[i+1][1],0+dp[i+1][0]);
+                }
+                dp[i][j]=profit;
+            }
+        }
+        return dp[0][1];
+    }
     int maxProfit(vector<int>& prices) {
         dp.resize(prices.size(),vector<int>(2,-1));
-        return maxp(prices,0,1);
+        // return maxp(prices,0,1);
+        return maxp1(prices);
     }
 };
