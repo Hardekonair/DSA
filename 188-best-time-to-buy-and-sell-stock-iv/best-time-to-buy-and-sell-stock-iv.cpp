1e8b011203dc77524b442f8973b56ece4aff1772
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-11-22 12:27:33 +0530

    Time: 7 ms (55.60%) | Memory: 18.3 MB (36.48%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -20,9 +20,47 @@ public:
         }
         return dp[i][buy][k]=profit;
     }
-    int maxProfit(int k, vector<int>& prices) {
-        dp.resize(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
 
-        return fn(prices,0,1,k);
+    int fn1(vector<int>& prices,int t){
+        // in dp size we do prices.size()+1, because we have a base case where i==n, return 0;
+        vector<vector<vector<int>>>dp(prices.size()+1,vector<vector<int>>(2,vector<int>(t+1,-1)));
+        // if(trans==0)
+        //     return 0;
+        for(int i=0;i<prices.size()+1;i++){
+            for(int j=0;j<2;j++){
+                dp[i][j][0]=0;
+            }
+        }
+
+        // if(i==prices.size())
+        //     return 0;
+        for(int j=0;j<2;j++){
+            for(int k=0;k<t+1;k++){
+                dp[prices.size()][j][k]=0;
+            }
+        }
+
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                for(int k=1;k<t+1;k++){
+                    int profit=0;
+                    if(j){
+                        profit=max(-prices[i]+dp[i+1][0][k],0+dp[i+1][1][k]);
+
+                    }
+                    else{
+                        profit=max(prices[i]+dp[i+1][1][k-1],0+dp[i+1][0][k]);
+                        
+                    }
+                    dp[i][j][k]=profit;
+                }
+            }
+        }
+        return dp[0][1][t];
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        // dp.resize(prices.size(),vector<vector<int>>(2,vector<int>(k+1,-1)));
+
+        return fn1(prices,k);
     }
 };
