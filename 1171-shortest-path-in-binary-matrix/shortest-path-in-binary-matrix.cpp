1bbfeb2ae50f31bb797586067f70a10fa12fa957
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2025-12-19 11:46:07 +0530

    Time: 12 ms (83.62%) | Memory: 25.3 MB (52.49%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {\
+        int n=grid.size();
+        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
+
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        dis[0][0]=1;
+
+        if(grid[0][0]==1 || grid[n-1][n-1]==1)  
+            return -1;
+            
+        vector<int> r={0,-1,-1,-1,0,1,1,1};
+        vector<int> c={-1,-1,0,1,1,1,0,-1};
+
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+
+            for(int k=0;k<8;k++){
+                int nr=x+r[k];
+                int nc=y+c[k];
+                if(nr==n-1 && nc==n-1)
+                    return 1+dis[x][y];
+                if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc]==0){
+                    if(dis[nr][nc]!=INT_MAX)    // FIRST REACHED DIS ALWAYS MIN IN BFS
+                        continue;
+                    else{
+                        dis[nr][nc]=1+dis[x][y];
+                        q.push({nr,nc});
+                    }
+                }
+            }
+        }
+        
+        if(dis[n-1][n-1]==INT_MAX)  
+            return -1;
+        return dis[n-1][n-1];
+
+    }
+};
