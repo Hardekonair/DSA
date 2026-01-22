0ea4afeecd5f35bd6f6a51aa2ade8d7289119e5c
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-22 12:18:31 +0530

    Time: 23 ms (92.46%) | Memory: 40.3 MB (71.57%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+    int find(int i,vector<int>& par){
+        if(par[i]==i)
+            return i;
+        return par[i]=find(par[i],par);
+    }
+
+    void uni(int i,int j,vector<int>& par){
+        int pari=find(i,par);
+        int parj=find(j,par);
+        if(pari==parj)  
+            return;
+        par[pari]=find(parj,par);
+        return;
+    }
+
+    vector<vector<string>> accountsMerge(vector<vector<string>>& acc) {
+        int n=acc.size();
+        unordered_map<string,int> mpp;
+
+        vector<int>par(n);
+        for(int i=0;i<n;i++){
+            par[i]=i;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=1;j<acc[i].size();j++){
+                if(mpp.find(acc[i][j])==mpp.end()){
+                    mpp[acc[i][j]]=i;
+                }
+                else{
+                    uni(i,mpp[acc[i][j]], par);
+                }
+            }
+        }
+        vector<vector<string>> vec(n);
+        for(auto it:mpp){
+            int i=find(it.second,par);
+            vec[i].push_back(it.first);
+        }
+        vector<vector<string>>ans;
+        for(int i=0;i<n;i++){
+            if(vec[i].size()==0)
+                continue;
+            
+            sort(vec[i].begin(),vec[i].end());
+            vector<string>temp;
+            temp.push_back(acc[i][0]);
+            for(auto it:vec[i]) 
+                temp.push_back(it);
+            
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
