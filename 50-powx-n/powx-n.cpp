05c6696a496d492a4e83fd2bd02944b998c8256a
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-26 02:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (13.77%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // double myPow(double x, int n) {
+    //     long long N=n;    // long long is used when n=INT_MIN but -INT_MIN is not in range
+    //     if(N<0){
+    //         x=1/x;
+    //         N=-N;
+    //     }
+    //     if(N==0)
+    //         return 1.0;
+        
+    //     if(N==1)
+    //         return x;
+        
+    //     if(N%2==0)
+    //     return myPow(x*x,N/2);
+    //     else
+    //     return x*myPow(x*x,(N-1)/2);
+    // }
+
+    double myPow(double x, int n) {
+        long long N=n;
+        double ans=1; 
+
+        if(x==-1)
+            return (n%2)?x:-x;
+        if(x==1)    
+            return x;
+
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        while(N>0){
+            if(N & 1)
+                ans*=x;
+            x*=x;
+            N>>=1;
+        }
+        return ans;
+    }
+};
