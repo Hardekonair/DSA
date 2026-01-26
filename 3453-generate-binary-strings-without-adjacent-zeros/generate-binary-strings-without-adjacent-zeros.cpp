79c1442dec27cd306519c5cdb74fc598f24911e5
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-26 22:38:50 +0530

    Time: 9 ms (80.77%) | Memory: 20.8 MB (84.42%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
-    void fn(string s,vector<string>& vec,int n){
+    // void fn(string s,vector<string>& vec,int n){
+    //     if(n==0){
+    //         vec.push_back(s);
+    //         return;
+    //     }
+    //     fn(s+'1',vec,n-1);
+    //     if(s.empty() || s.back()!='0')
+    //         fn(s+'0',vec,n-1);
+    // }
+    void fn(string& s,vector<string>& vec,int n){
         if(n==0){
             vec.push_back(s);
             return;
         }
-        fn(s+'1',vec,n-1);
-        if(s.empty() || s.back()!='0')
-            fn(s+'0',vec,n-1);
+        s.push_back('1');
+        fn(s,vec,n-1);
+        s.pop_back();
+        if(s.empty() || s.back()!='0'){
+            s.push_back('0');
+            fn(s,vec,n-1);
+            s.pop_back();
+        }
     }
     vector<string> validStrings(int n) {
         vector<string> vec;
