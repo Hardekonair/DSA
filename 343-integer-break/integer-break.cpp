cca4d313b549633de9e958c95fb18e399205d48b
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-28 02:16:36 +0530

    Time: 1 ms (11.83%) | Memory: 8.6 MB (40.99%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int>dp;
+    int fn(int n){
+        if(n==1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        int res=INT_MIN;
+        for(int i=1;i<n;i++){
+            int prod=max(i*(n-i),i*fn(n-i));
+            res=max(res,prod);
+        }
+        return dp[n]=res;
+    }
+    int integerBreak(int n) {
+        dp.resize(n+1,-1);
+        return fn(n);
+    }
+};
