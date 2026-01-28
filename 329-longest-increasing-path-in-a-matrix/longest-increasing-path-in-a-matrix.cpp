22abf107d5a417838edb47d0e42e549ee1de7cad
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-28 16:10:26 +0530

    Time: 241 ms (7.06%) | Memory: 75.3 MB (5.00%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -2,31 +2,78 @@ class Solution {
 public:
     vector<vector<int>>d={{1,0},{0,1},{-1,0},{0,-1}};
     vector<vector<int>>dp;
-    int dfs(int i,int j,vector<vector<int>>& matrix){
+    // int dfs(int i,int j,vector<vector<int>>& matrix){
 
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int ans=1;
-        for(int k=0;k<4;k++){
-            int ni=i+d[k][0];
-            int nj=j+d[k][1];
-            if(ni>=0 && ni<n && nj>=0 && nj<m && matrix[ni][nj]>matrix[i][j]){
-                ans=max(ans,1+dfs(ni,nj,matrix));
-            }
-        }
-        return dp[i][j]=ans;
-    }
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     int n=matrix.size();
+    //     int m=matrix[0].size();
+    //     int ans=1;
+    //     for(int k=0;k<4;k++){
+    //         int ni=i+d[k][0];
+    //         int nj=j+d[k][1];
+    //         if(ni>=0 && ni<n && nj>=0 && nj<m && matrix[ni][nj]>matrix[i][j]){
+    //             ans=max(ans,1+dfs(ni,nj,matrix));
+    //         }
+    //     }
+    //     return dp[i][j]=ans;
+    // }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        dp.resize(n,vector<int>(m,-1));
-        int ans=0;
+        // dp.resize(n,vector<int>(m,-1));
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         ans=max(ans,dfs(i,j,matrix));
+        //     }
+        // }
+        // return ans;
+
+
+                vector<vector<int>>dp(n,vector<int>(m,1)),indeg(n,vector<int>(m,0));
+        
+        vector<vector<int>>d={{0,1},{1,0},{0,-1},{-1,0}};
+        //1 valv indegeree
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans=max(ans,dfs(i,j,matrix));
+                for(auto it:d){
+                    int ni=i+it[0],nj=j+it[1];
+                    
+                    if(ni>=0 && ni<n && nj>=0 && nj<m && matrix[ni][nj]<matrix[i][j]){
+                        indeg[i][j]++;
+                    }
+                }
+                    
             }
+        }
+        //2. insett in queeu
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(indeg[i][j]==0)    
+                    q.push({i,j});
+            }
+        }
+        
+        // 3. topological bfs
+        int ans=1;
+        while(!q.empty()){
+            auto [i,j]=q.front();
+            q.pop();
+            
+            for(auto it:d){
+                int ni=i+it[0],nj=j+it[1];
+                    
+                if(ni>=0 && ni<n && nj>=0 && nj<m && matrix[ni][nj]>matrix[i][j] && indeg[ni][nj]!=0){
+                    dp[ni][nj]=max(dp[ni][nj],1+dp[i][j]);
+                    indeg[ni][nj]--;
+                    
+                    if(indeg[ni][nj]==0)
+                        q.push({ni,nj});
+                }
+            }
+            ans=max(ans,dp[i][j]);
         }
         return ans;
     }
