ee5e493a999aeb6b8b643bc4007f9a5f6a191750
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-29 22:39:39 +0530

    Time: 3 ms (54.25%) | Memory: 14.1 MB (75.71%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    void findcomb(int i,int t,vector<int>& arr, vector<int>& temp,vector<vector<int>>& ans){
-        if(i==arr.size()){
-            if(t==0)
+    void combsum(int i,int target,vector<int>& temp,vector<vector<int>>& ans,vector<int>& arr){
+        // if(target==0){
+        //     ans.push_back(temp);
+        //     return;
+        // }
+        if(i<0){
+            if(target==0){
                 ans.push_back(temp);
-        return;
+            }
+            return;
         }
 
-        if(arr[i]<=t){
+        if(arr[i]<=target){
             temp.push_back(arr[i]);
-            findcomb(i,t-arr[i],arr,temp,ans);
+            combsum(i,target-arr[i],temp,ans,arr);
             temp.pop_back();
         }
-        findcomb(i+1,t,arr,temp,ans);
+        combsum(i-1,target,temp,ans,arr);
     }
+
+    // void findcomb(int i,int t,vector<int>& arr, vector<int>& temp,vector<vector<int>>& ans){
+    //     if(i==arr.size()){
+    //         if(t==0)
+    //             ans.push_back(temp);
+    //     return;
+    //     }
+
+    //     if(arr[i]<=t){
+    //         temp.push_back(arr[i]);
+    //         findcomb(i,t-arr[i],arr,temp,ans);
+    //         temp.pop_back();
+    //     }
+    //     findcomb(i+1,t,arr,temp,ans);
+    // }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        int i=0;
+        int n=candidates.size();
         
-        findcomb(i,target,candidates,temp,ans);
+        combsum(n-1,target,temp,ans,candidates);
 
         return ans;
         
