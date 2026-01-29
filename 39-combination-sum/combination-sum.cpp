0f4c6ac54e3a0a08f5036d7c05f02a4664fde425
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-29 22:41:39 +0530

    Time: 7 ms (27.77%) | Memory: 17.9 MB (25.65%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
-    void combsum(int i,int target,vector<int>& temp,vector<vector<int>>& ans,vector<int>& arr){
-        if(i<0){
-            if(target==0){
+    void combsum(int i, int target, vector<int>& temp, vector<vector<int>>& ans,
+                 vector<int>& arr) {
+        // if(i<0){
+        //     if(target==0){
+        //         ans.push_back(temp);
+        //     }
+        //     return;
+        // }
+
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (i == 0) {
+            if (target % arr[0] == 0) {
+                int cnt = target / arr[0];
+                for (int k = 0; k < cnt; k++)
+                    temp.push_back(arr[0]);
+
                 ans.push_back(temp);
+
+                for (int k = 0; k < cnt; k++)
+                    temp.pop_back(); // backtrack
             }
             return;
         }
 
-        combsum(i-1,target,temp,ans,arr);
-        if(arr[i]<=target){
+        combsum(i - 1, target, temp, ans, arr);
+        if (arr[i] <= target) {
             temp.push_back(arr[i]);
-            combsum(i,target-arr[i],temp,ans,arr);
+            combsum(i, target - arr[i], temp, ans, arr);
             temp.pop_back();
         }
     }
 
-    // void findcomb(int i,int t,vector<int>& arr, vector<int>& temp,vector<vector<int>>& ans){
+    // void findcomb(int i,int t,vector<int>& arr, vector<int>&
+    // temp,vector<vector<int>>& ans){
     //     if(i==arr.size()){
     //         if(t==0)
     //             ans.push_back(temp);
@@ -33,11 +54,10 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        int n=candidates.size();
-        
-        combsum(n-1,target,temp,ans,candidates);
+        int n = candidates.size();
+
+        combsum(n - 1, target, temp, ans, candidates);
 
         return ans;
-        
     }
 };
