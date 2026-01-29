66ca938f433b28795d877500b58629d17802dc81
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-30 01:08:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (98.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    void combsum2(int i,int t,vector<int>& arr,vector<int>& temp,vector<vector<int>>& ans){
+        if(t==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(i<0 || t<0)
+            return;
+        
+        temp.push_back(arr[i]);
+        combsum2(i-1,t-arr[i],arr,temp,ans);
+        temp.pop_back();
+        int j=i-1;
+        while(j>=0 && arr[j]==arr[i])
+            j--;
+        if(j>=0){
+            combsum2(j,t,arr,temp,ans);
+        }
+    }
     void solve(int i,int target,vector<int>& arr,vector<int> &temp,vector<vector<int>> &ans){
         if(target==0){
             ans.push_back(temp);
@@ -16,10 +34,12 @@ public:
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        int n=candidates.size();
         sort(candidates.begin(),candidates.end());
         vector<int> temp;
         vector<vector<int>> ans;
-        solve(0,target,candidates,temp,ans);
+        // solve(0,target,candidates,temp,ans);
+        combsum2(n-1,target,candidates,temp,ans);
         return ans;
 
         
