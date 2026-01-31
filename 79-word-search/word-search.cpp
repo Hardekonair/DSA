214d27a3fbe6c4afb89db4735552e3aef78dfd62
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-01-31 17:20:56 +0530

    Time: 903 ms (9.74%) | Memory: 11.8 MB (20.44%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool isvalid(int i,int j,vector<vector<char>>& board,vector<vector<int>>& vis){
+        int n=board.size(), m=board[0].size();
+        if((i<n && i>=0) && (j<m && j>=0) && vis[i][j]==0)
+            return true;
+        return false;
+    }
+
+    bool wordsearch(int i,int j,int k,string& word,vector<vector<char>>& board,vector<vector<int>>& vis){
+        if(k==word.size()-1 ){
+            return true;
+        }
+        vis[i][j]=1;
+        // bool ans=false;
+        vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        
+        for(int x=0;x<4;x++){
+            int ni=i+dir[x][0];
+            int nj=j+dir[x][1];
+            if(isvalid(ni,nj,board,vis) && word[k+1]==board[ni][nj]){
+                if(wordsearch(ni,nj,k+1,word,board,vis)){
+                    vis[ni][nj]=0;
+                    return true;
+                }
+            }
+        }
+        vis[i][j]=0;
+        return false;
+    }
+
+    bool solve(int r,int c,int k,string s,vector<vector<char>>& board,vector<vector<int>>& vis){
+        if(k==s.size())
+            return true;
+
+        if(!isvalid(r,c,board,vis) || board[r][c]!=s[k])   
+            return false;
+
+        vis[r][c]=1;
+
+        int i=r,j=c;
+        bool ans=solve(i,j-1,k+1,s,board,vis) || solve(i-1,j,k+1,s,board,vis) || solve(i+1,j,k+1,s,board,vis) || solve(i,j+1,k+1,s,board,vis);
+
+        vis[i][j]=0;
+        return ans;
+    }
+    
+    bool exist(vector<vector<char>>& board,string s){
+        vector<vector<int>> vis(board.size(), vector<int>(board[0].size(), 0));
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==s[0])
+                    // if(wordsearch(i,j,0,s,board,vis))
+                    if(solve(i,j,0,s,board,vis))
+                        return true;
+                
+            }
+        }
+        return false;
+    }
+};
