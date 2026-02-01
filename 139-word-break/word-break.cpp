f9acbdac3031830a6d481ddd4f852ff2385498e2
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-01 12:21:31 +0530

    Time: 3 ms (69.20%) | Memory: 13.7 MB (52.37%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<int>dp;
-    bool ansbreak1(int i,string s,unordered_set<string>& mpp){
-        if(i<0){
-            return true;
-        }
-        if(dp[i]!=-1)
-            return dp[i];
-        for(int k=i;k>=0;k--){
-            string temp=s.substr(k,i-k+1);
-            if(mpp.find(temp)!= mpp.end() && ansbreak1(k-1,s,mpp)){
-                return dp[i]=true;
-            }
-        }
-        return dp[i]=false;
-    }
+    // bool ansbreak1(int i,string s,unordered_set<string>& mpp){
+    //     if(i<0){
+    //         return true;
+    //     }
+    //     if(dp[i]!=-1)
+    //         return dp[i];
+    //     for(int k=i;k>=0;k--){
+    //         string temp=s.substr(k,i-k+1);
+    //         if(mpp.find(temp)!= mpp.end() && ansbreak1(k-1,s,mpp)){
+    //             return dp[i]=true;
+    //         }
+    //     }
+    //     return dp[i]=false;
+    // }
     bool ansbreak(int i,string s,unordered_set<string>& mpp){
         if(i==s.size()){
             return true;;
         }
         if(dp[i]!=-1)
             return dp[i];
+        string temp="";
         for(int k=i;k<s.size();k++){
-            string temp=s.substr(i,k-i+1);
+            // string temp=s.substr(i,k-i+1);
+            temp+=s[k];
             if(mpp.find(temp)!= mpp.end() && ansbreak(k+1,s,mpp)){
                 return dp[i]=true;
             }
@@ -45,8 +47,8 @@ public:
         unordered_set<string> st(wordDict.begin(),wordDict.end());
         int n=s.size();
         dp.resize(n,-1);
-        return ansbreak1(n-1,s,st);
-        // return ansbreak(0,s,st);
+        // return ansbreak1(n-1,s,st);
+        return ansbreak(0,s,st);
         
     }
 };
