b99288c4d5d3c6253098a0125f0045c182996d11
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-01 12:19:17 +0530

    Time: 11 ms (44.97%) | Memory: 16.2 MB (36.04%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int>dp;
     bool ansbreak1(int i,string s,unordered_set<string>& mpp){
-        if(i==0){
-            return (mpp.find(to_string(s[0]))!=mpp.end());
+        if(i<0){
+            return true;
         }
         if(dp[i]!=-1)
             return dp[i];
         for(int k=i;k>=0;k--){
-            string temp=s.substr(k,k-i+1);
-            if(mpp.find(temp)!= mpp.end() && ansbreak(k-1,s,mpp)){
-                return dp[k]=true;
+            string temp=s.substr(k,i-k+1);
+            if(mpp.find(temp)!= mpp.end() && ansbreak1(k-1,s,mpp)){
+                return dp[i]=true;
             }
         }
         return dp[i]=false;
@@ -45,8 +45,8 @@ public:
         unordered_set<string> st(wordDict.begin(),wordDict.end());
         int n=s.size();
         dp.resize(n,-1);
-        // return ansbreak1(n-1,s,st);
-        return ansbreak(0,s,st);
+        return ansbreak1(n-1,s,st);
+        // return ansbreak(0,s,st);
         
     }
 };
