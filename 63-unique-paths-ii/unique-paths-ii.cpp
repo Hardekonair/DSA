6de7dc732a1f9b288abf6348ebd7678fb01cefcf
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-03 15:10:37 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (29.97%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -37,6 +37,29 @@ public:
     }
 
                                 // TABULATION - NON RECURSIVE
+    int fn(vector<vector<int>>& grid,int m,int n){
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        if(grid[0][0]==1)
+            return 0;
+        dp[0][0]=1;
+        for(int i=1;i<m;i++){
+            if(grid[i][0]==0)
+                dp[i][0]=dp[i-1][0];
+        }
+        for(int i=1;i<n;i++){
+            if(grid[0][i]==0)
+                dp[0][i]=dp[0][i-1];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(grid[i][j]==1)
+                    continue;
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
     int helper3(vector<vector<int>>& grid,int m,int n){
         vector<vector<int>> dp(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
@@ -83,6 +106,7 @@ public:
         int m=grid.size(),n=grid[0].size();
         dp.resize(m,vector<int>(n,-1));
 
-        return helper3(grid,grid.size(),grid[0].size());
+        // return helper3(grid,grid.size(),grid[0].size());
+        return fn(grid,m,n);
     }
 };
