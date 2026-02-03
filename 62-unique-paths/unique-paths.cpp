05bc1885846aab99525c37739cb896e60c4ea109
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-03 15:07:04 +0530

    Time: 4 ms (0.96%) | Memory: 13.1 MB (5.07%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -58,11 +58,12 @@ public:
     }
     int fn(int m,int n){
         vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            dp[i][0]=1;
+        dp[0][0]=1;
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0];
         }
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1];
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
