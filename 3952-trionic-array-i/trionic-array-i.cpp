d2632ac575b1be6d88ed672624f7f44f91363fd7
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-04 11:02:52 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (35.37%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -6,26 +6,19 @@ public:
         bool temp=false;
         int i=0;
         
-        while(i<n-1 && count==3 && nums[i]<nums[i+1]){
-        temp=true;
-        i++;}
-        if(temp)count--;
+        while(i<n-1 && nums[i]<nums[i+1])   i++;
+        if(i==0 || i==n-1)    // NO/ALL INCREASING
+            return false;
 
-        while(i<n-1 && count==2 && nums[i]>nums[i+1]){
-            temp=false;
-            i++;
-        }
-        if(temp==false) count--;
+        int mid=i;
+        while(i<n-1 && nums[i]>nums[i+1])   i++;
+        if(mid==i || i==n-1)  // NO/ALL DECREASING
+            return false;
 
-        while(i<n-1 && count==1 && nums[i]<nums[i+1]){
-            i++;
-            temp=true;
-        }
-        if(temp)
-            count--;
+        while(i<n-1 && nums[i]<nums[i+1])   i++;
+        if(i!=n-1)
+            return false;
 
-        if(count==0 && i==n-1) return true;
-        return false;
-
+        return true;
     }
 };
