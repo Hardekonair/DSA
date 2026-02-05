9d01ab425658cdc0dcd3410df2640637120b541c
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-05 19:31:28 +0530

    Time: 172 ms (21.75%) | Memory: 204.1 MB (7.94%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -14,36 +14,19 @@ public:
             return dp[i][s1];
         return dp[i][s1]=equalpart(i-1,s1,s2,arr) || equalpart(i-1,s1+arr[i],s2-arr[i],arr);
     }
-    bool equalsubset(vector<int>& arr, int i, int s,int s2){
+    bool equalsubset(vector<int>& arr, int i, int s2,int s){
         if(s2==(s/2))
             return true;
-        if(i<0)
-            return false;
+        if(i==0)
+            return (s2+arr[i]==(s/2));
         if(dp[i][s2]!=-1)
             return dp[i][s2];
         
-        bool pick=false,notpick=false;
-        if(s2<=(s/2))
-            pick=equalsubset(arr,i-1,s,s2+arr[i]);
-        notpick=equalsubset(arr,i-1,s,s2);
+        bool pick=false,notpick=equalsubset(arr,i-1,s2,s);
+        if(s2 + arr[i]<=(s/2))
+            pick=equalsubset(arr,i-1,s2+arr[i],s);
 
-        return dp[i][s2]=(pick | notpick);
-    }
-    // LEFT TO RIGHT APPROACH
-    bool equalsubset1(vector<int>& arr, int i, int s1,int s2){
-        if(s2==s1)
-            return true;
-        if(i<0)
-            return false;
-        if(dp[i][s1]!=-1)
-            return dp[i][s1];
-        
-        bool pick=false,notpick=false;
-        if(s2<=s1)
-            pick=equalsubset1(arr,i-1,s1-arr[i],s2+arr[i]);
-        notpick=equalsubset1(arr,i-1,s1,s2);
-
-        return dp[i][s1]=(pick | notpick);
+        return dp[i][s2]=(pick || notpick);
     }
 
     
@@ -51,10 +34,10 @@ public:
         int s1=0,s2=0;
         for(auto it:nums)
             s2+=it;
-        // if(s1%2!=0)
-        //     return false;
+        if(s2%2!=0)
+            return false;
         dp.resize(nums.size(),vector<int>(s2+1,-1));
-        // return equalsubset1(nums,nums.size()-1,s1,s2);
-        return equalpart(nums.size()-1,s1,s2,nums);
+        return equalsubset(nums,nums.size()-1,s1,s2);
+        // return equalpart(nums.size()-1,s1,s2,nums);
     }
 };
