7b298a5e7157ee0c826f444c9a02c9874e23e7d6
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-05 19:04:50 +0530

    Time: 170 ms (23.40%) | Memory: 262.4 MB (5.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     vector<vector<int>>dp;
+    // RIGHT TO LEFT APRAOCH
+    bool equalpart(int i,int s1,int s2,vector<int>& arr){
+        if(s1==s2)
+            return true;
+        if(s2<s1)
+            return false;
+        if(i==0){
+            return (s1 + arr[0] == s2 - arr[0]);
+        }
+        if(dp[i][s1]!=-1)
+            return dp[i][s1];
+        return dp[i][s1]=equalpart(i-1,s1,s2,arr) || equalpart(i-1,s1+arr[i],s2-arr[i],arr);
+    }
     bool equalsubset(vector<int>& arr, int i, int s,int s2){
         if(s2==(s/2))
             return true;
@@ -16,6 +29,7 @@ public:
 
         return dp[i][s2]=(pick | notpick);
     }
+    // LEFT TO RIGHT APPROACH
     bool equalsubset1(vector<int>& arr, int i, int s1,int s2){
         if(s2==s1)
             return true;
@@ -31,13 +45,16 @@ public:
 
         return dp[i][s1]=(pick | notpick);
     }
+
+    
     bool canPartition(vector<int>& nums) {
         int s1=0,s2=0;
         for(auto it:nums)
-            s1+=it;
-        if(s1%2!=0)
-            return false;
-        dp.resize(nums.size(),vector<int>(s1+1,-1));
-        return equalsubset1(nums,nums.size()-1,s1,s2);
+            s2+=it;
+        // if(s1%2!=0)
+        //     return false;
+        dp.resize(nums.size(),vector<int>(s2+1,-1));
+        // return equalsubset1(nums,nums.size()-1,s1,s2);
+        return equalpart(nums.size()-1,s1,s2,nums);
     }
 };
