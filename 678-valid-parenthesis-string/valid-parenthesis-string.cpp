db4e39fa36f7308476dc8e1cc91d3cdcad421874
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-06 10:18:49 +0530

    Time: 15 ms (5.01%) | Memory: 25.4 MB (5.01%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool valid(int i,int open,string s){
-        if(open<0)
+    bool valid(int i,int close,string s){
+        if(close<0)
             return false;
-        if(i==s.size()){
-            if(open!=0)
-                return false;
-            return true;
+        if(i==0){
+            if((s[i]=='(' && close==1) || (s[i]=='*' && close<=1))
+                return true;
+            return false;
         }
-        if(dp[i][open]!=-1)
-            return dp[i][open];
-        if(s[i]=='(')
-            return dp[i][open]=valid(i+1,open+1,s);
-        else if(s[i]==')'){
-            if(open==0)
+        if(dp[i][close]!=-1)
+            return dp[i][close];
+        if(s[i]=='('){
+            if(close==0)
                 return false;
-            return dp[i][open]=valid(i+1,open-1,s);}
+            return dp[i][close]=valid(i-1,close-1,s);
+        }
+        else if(s[i]==')')
+            return dp[i][close]=valid(i-1,close+1,s);
         else 
-            return dp[i][open]=valid(i+1,open+1,s) || valid(i+1,open,s) || valid(i+1,open-1,s);
+            return dp[i][close]=valid(i-1,close+1,s) || valid(i-1,close,s) || valid(i-1,close-1,s);
         
     }
     bool checkValidString(string s) {
         int o=0;
         dp.resize(s.size(),vector<int>(s.size(),-1));
-        return valid(0,o,s);
+        return valid(s.size()-1,o,s);
     }
 };
