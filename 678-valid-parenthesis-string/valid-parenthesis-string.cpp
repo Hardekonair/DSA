8d4d0387066602926b60aa7e743b884b1529b2da
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-06 10:56:13 +0530

    Time: 3 ms (8.68%) | Memory: 12.7 MB (5.01%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -22,9 +22,40 @@ public:
             return dp[i][close]=valid(i-1,close+1,s) || valid(i-1,close,s) || valid(i-1,close-1,s);
         
     }
+    bool valid1(string& s){
+        int n=s.size();
+        vector<vector<int>>dp(s.size(),vector<int>(s.size()+1,0));
+        // if(close<0)
+        //     return false;
+        // if(i==0){
+        //     if((s[i]=='(' && close==1) || (s[i]=='*' && close<=1))
+        //         return true;
+        //     return false;
+        // }
+        if(s[0]=='(') 
+            dp[0][1]=1;
+        if (s[0]=='*'){
+            dp[0][1]=dp[0][0]=1;
+        }
+
+        for(int i=1;i<n;i++){                 // since valid start from n-1 in memo
+            for(int close=0;close<n;close++){
+                if(s[i]=='('){
+                    if(close>0)
+                        dp[i][close]=dp[i-1][close-1];
+                }
+                else if(s[i]==')')
+                    dp[i][close]=dp[i-1][close+1];
+                else 
+                    dp[i][close]=dp[i-1][close+1] || dp[i-1][close] || (close>0?dp[i-1][close-1]:0);
+            }
+        }
+        return dp[n-1][0];        
+    }
     bool checkValidString(string s) {
         int o=0;
         dp.resize(s.size(),vector<int>(s.size(),-1));
-        return valid(s.size()-1,o,s);
+        // return valid(s.size()-1,o,s);
+        return valid1(s);
     }
 };
