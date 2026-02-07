236aafc7fc0993ec77022af30639a216bc01eece
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-07 12:40:32 +0530

    Time: 14 ms (53.15%) | Memory: 41.1 MB (18.38%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int helper(vector<int>& nums,int target,int i,int c){
+    // RECURSION
+    int helper1(vector<int>& nums,int target,int i){
+        if(target<0)
+            return 0;
         if(i<0){
-            if(target==c)
+            if(target==0)
                 return 1;
             return 0;
         }
-        if(dp[i][c]!=-1)
-            return dp[i][c];
 
-        int pos=helper(nums,target,i-1,c+nums[i]);
-        int neg=helper(nums,target,i-1,c-nums[i]);
+        int pos=helper1(nums,target-nums[i],i-1);
+        int neg=helper1(nums,target+nums[i],i-1);
 
-        return dp[i][c]=pos+neg;
+        return pos+neg;
     }
+    // MEMOIZATION
+    int helper2(vector<int>& nums,int target,int i){
+        if(target<0 || target>2000) 
+            return 0;
+        if(i<0){
+            if(target==1000)
+                return 1;
+            return 0;
+        }
+        if(dp[i][target]!=-1)
+            return dp[i][target];
+
+        int pos=helper2(nums,target-nums[i],i-1);
+        int neg=helper2(nums,target+nums[i],i-1);
+
+        return dp[i][target]=pos+neg;
+    }
+    // TABULATION
+    
     int findTargetSumWays(vector<int>& nums, int target) {
         int c=1000;
         dp.resize(nums.size(),vector<int>(2001,-1));
-        return helper(nums,target+1000,nums.size()-1,c);
+        // return helper(nums,target+1000,nums.size()-1,c);
+        return helper2(nums,target+1000,nums.size()-1);
     }
 };
