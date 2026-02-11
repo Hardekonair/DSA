f2aea9d1779b8d9aec7e579ff8eccc73c4b95c1f
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-11 14:10:36 +0530

    Time: 18 ms (5.62%) | Memory: 27.2 MB (5.42%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,15 +7,14 @@ public:
         
         if(dp[i][buy]!=-1)
             return dp[i][buy];
-        int profit=0;
-        if(buy){
-            profit=max(-prices[i]+maxp(prices,i+1,0),0+maxp(prices,i+1,1));
 
+        if(i!=prices.size() && buy){    // CAN BUY      return  max(dont buy and move . buy and move)
+            return dp[i][buy]=max(-0+maxp(prices,i+1,1),-prices[i]+maxp(prices,i+1,0));
         }
-        else if(buy==0){
-            profit=max(prices[i]+maxp(prices,i+1,1),maxp(prices,i+1,0));
+        else {    // CAN'T BUY    return max(sell and move, dont sell and move)
+            return dp[i][buy]=max(prices[i]+maxp(prices,i+1,1),maxp(prices,i+1,0));
         }
-        return dp[i][buy]=profit;
+
     }
 
     int maxp1(vector<int>& prices){
@@ -84,8 +83,8 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         dp.resize(prices.size(),vector<int>(2,-1));
-        // return maxp(prices,0,1);
+        return maxp(prices,0,1);
         // return maxp1(prices);
-        return maxp3(prices);
+        // return maxp3(prices);
     }
 };
