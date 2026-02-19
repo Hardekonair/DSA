d8eab3f710d4c2d9228f6a9343cc85b18e8a771d
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-19 15:32:00 +0530

    Time: 87 ms (92.55%) | Memory: 16 MB (7.66%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -14,11 +14,27 @@ public:
         }
         return dp[i][j]=ans;
     }
+    int fn(vector<int>& cuts){
+        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),0));
+        for(int i=cuts.size()-1;i>=0;i--){
+            for(int j=0;j<cuts.size();j++){
+            if(i+1>j-1) continue;
+            int ans=0;
+            for(int k=i+1;k<j;k++){
+                int cost=cuts[i]*cuts[j]*cuts[k]+dp[i][k]+dp[k][j];
+                ans=max(ans,cost);
+            }
+            dp[i][j]=ans;
+            }
+        }
+        return dp[0][cuts.size()-1];
+    }
     int maxCoins(vector<int>& nums) {
         int n=nums.size();
         nums.insert(nums.begin()+0,1);
         nums.push_back(1);
         dp.resize(nums.size(),vector<int>(nums.size(),-1));
-        return fn1(0,nums.size()-1,nums);
+        // return fn1(0,nums.size()-1,nums);
+        return fn(nums);
     }
 };
