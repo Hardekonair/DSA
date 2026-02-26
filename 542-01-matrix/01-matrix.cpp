7ee78717da84eaba19e67f6abf76812fd50dd103
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-26 19:11:25 +0530

    Time: 23 ms (41.24%) | Memory: 34.8 MB (74.01%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
 
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
+        vector<vector<int>>res(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;  // FOR STORING ROW AND COL
 
-        queue<pair<int,int>>q;
-        vector<vector<int>> ans(n,(vector<int>(m,-1)));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    ans[i][j]=0;
-
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    res[i][j]=0;
                     q.push({i,j});
                 }
             }
         }
-
+        vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            
-            vector<int> tr={-1,1,0,0};
-            vector<int> tc={0,0,-1,1};
-            for(int k=0;k<4;k++){
-                int nr=r+tr[k];
-                int nc=c+tc[k];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && ans[nr][nc]==-1){
-                    ans[nr][nc]=1+ans[r][c];
-                    
-                    q.push({nr,nc});
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                int r=q.front().first;
+                int c=q.front().second;
+                q.pop();
+                for(int p=0;p<4;p++){
+                    int nr=r+dir[p][0], nc=c+dir[p][1];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && res[nr][nc]==-1){
+                        res[nr][nc]=1+res[r][c];
+                        q.push({nr,nc});
+                    }
                 }
+                
             }
         }
-        return ans;
+        return res;
 
     }
 };
