e081f4ea0bcc9b29d3fce715155b3559761143a9
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-02-26 17:47:44 +0530

    Time: 3 ms (77.10%) | Memory: 20.2 MB (27.13%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    bool dfs(int i,vector<vector<int>>& adj,vector<int>& vis){
+        if(vis[i]==2)   return false;
+        if(vis[i]==1)   return true;
+        vis[i]=1;
+
+        for(auto it:adj[i]){
+                if(dfs(it,adj,vis))
+                    return true;
+        }
+        vis[i]=2;
+        return false;
+    }
+                                    // HIDDEN CONCEPT IS TO FIND IF CYCLE EXIST OR NOT
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
                                     //  USING TOPOSORT
 
@@ -14,30 +27,37 @@ public:
         }
         
         // STORING INDEGREE OF 0, THAT WILL START THE COURSES
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
-        }
+        // queue<int>q;
+        // for(int i=0;i<n;i++){
+        //     if(indegree[i]==0){
+        //         q.push(i);
+        //     }
+        // }
 
         // TRAVERSING ZERO INDEGREE AND REDUCING CONNECTED COMP DEGREE
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
+        // while(!q.empty()){
+        //     int node=q.front();
+        //     q.pop();
 
-            for(auto it:adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0)
-                    q.push(it);
-            }
-        }
+        //     for(auto it:adj[node]){
+        //         indegree[it]--;
+        //         if(indegree[it]==0)
+        //             q.push(it);
+        //     }
+        // }
 
         // CHECKING IF ALL COURSES ARE NOT VISITED = IF ALL INDEGREE 0 OR NOT
-        for (auto deg: indegree){
-            if(deg!=0){
+        // for (auto deg: indegree){
+        //     if(deg!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        vector<int>vis(n,0);
+        for(int i=0;i<n;i++){
+            if(dfs(i,adj,vis))
                 return false;
-            }
         }
         return true;
     }
