5dfdbdd88085c992b8ee3134ed77c0d1c1bf8aba
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-01 19:32:53 +0530

    Time: 2902 ms (5.01%) | Memory: 20.7 MB (76.09%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    int ladderLength(string sw, string ew, vector<string>& word) {
-        queue<pair<string,int>> q;
-        q.push({sw,1});
-        
-        unordered_set<string> st(word.begin(),word.end());
-        st.erase(sw);
-        while(!q.empty()){
-            string w=q.front().first;
-            int step=q.front().second;
-            q.pop();
-            if(w==ew)
-                return step;
-            for(int i=0;i<w.size();i++){
-                char org=w[i];
-                for(char it='a';it<='z';it++){
-                    w[i]=it;
-                    if(st.find(w)!=st.end()){
-                        q.push({w,step+1});
-                        st.erase(w);
-                    }
-
-                }
-                w[i]=org;
-            }
+    bool isvalid(string a,string b){
+        if(a.size()!=b.size() || a==b)  
+            return false;
+        int cnt=0;
+        for(int i=0;i<a.size();i++){
+            if(a[i]!=b[i])
+                cnt++;
         }
-        return 0;
+        return cnt==1?true:false;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<string> q;
+        unordered_map<string,int>mpp;
+        for(auto it:wordList){
+            mpp[it]=0;
+        }
+        q.push(beginWord);
+
+        int len=1;
+        bool found=false;
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                string word=q.front();
+                q.pop();
+                // if(isvalid(word,endWord) && mpp.find(endWord)!=mpp.end()){
+                //     found=true;
+                //     break;
+                // }
+                for(auto it:mpp){
+                    if(isvalid(word,it.first) && it.second==0){
+                        if(it.first==endWord){
+                            return len+1;
+                        }
+                        q.push(it.first);
+                        mpp[it.first]=1;
+                    }
+                }
+            }
+            len++;
+        }
+        if(!found)  return 0;
+        return len-1;
     }
 };
