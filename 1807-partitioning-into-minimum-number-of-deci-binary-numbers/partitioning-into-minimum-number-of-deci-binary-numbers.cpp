e41f23ad36f01a1e1090892e73e141a29cb3251a
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-01 18:18:32 +0530

    Time: 154 ms (5.68%) | Memory: 72.3 MB (16.53%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // void bin(int n,string temp,vector<string>& ans){
+    //     if(temp.size()==n){
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     bin(n,temp+'1',ans);
+    //     bin(n,temp+'0',ans);
+    // }
+
+    int fn(string n){
+        if(n==""){
+            return 0;
+        }
+        string temp="";
+        for(int i=0;i<n.size();i++){
+            if(n[i]=='0')
+                temp.push_back('0');
+            else
+                temp.push_back('1');
+        }
+        string ans="";
+        for(int i=0;i<n.size();i++){
+            int ch=(n[i]-'0')-(temp[i]-'0');
+
+            if(ch==0 && ans.empty())  continue;
+            ans.push_back(ch+'0');
+        }
+        return 1+fn(ans);
+    }
+
+    int minPartitions(string n) {
+        return fn(n);
+    }
+};
