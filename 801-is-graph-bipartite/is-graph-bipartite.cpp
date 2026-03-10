c88e321b00edeeeba2a63ae146cc3e0226f4c315
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-10 19:24:31 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (56.61%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:         
                                             // DFS APPROACH
-    // bool isBipartite(vector<vector<int>>& graph) {
-        
-    // }
-                                            // BFS APPROACH
+    bool dfs(int i,int c,vector<int>& color,vector<vector<int>>& graph){
+        color[i]=c;
+        for(auto it:graph[i]){
+            if(color[it]==color[i]) return false;
+            else if(color[it]==-1){
+                if(dfs(it,1-c,color,graph)==false)  
+                    return false;
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>color(n,-1);
 
         for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                queue<int>q;
-                q.push(i);
-                
-                while(!q.empty()){
-                    int node=q.front();
-                    q.pop();
-                    for(auto it:graph[node]){
-                        if(color[it]==-1){
-                            color[it]=1-color[node];
-                            q.push(it);
-                        }
-                        else if(color[it]==color[node]) 
-                            return false;
-                    }
-                }
-            }
+            if(color[i]==-1)
+                if(dfs(i,0,color,graph)==false)
+                    return false;
         }
         return true;
     }
+                                            // BFS APPROACH
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int n=graph.size();
+    //     vector<int>color(n,-1);
+
+    //     for(int i=0;i<n;i++){
+    //         if(color[i]==-1){
+    //             queue<int>q;
+    //             q.push(i);
+                
+    //             while(!q.empty()){
+    //                 int node=q.front();
+    //                 q.pop();
+    //                 for(auto it:graph[node]){
+    //                     if(color[it]==-1){
+    //                         color[it]=1-color[node];
+    //                         q.push(it);
+    //                     }
+    //                     else if(color[it]==color[node]) 
+    //                         return false;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
 };
