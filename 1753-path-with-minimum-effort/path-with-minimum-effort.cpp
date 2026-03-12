1d19a22244770b2cfd4d17e748da9c217e788fb5
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-12 18:34:38 +0530

    Time: 191 ms (11.76%) | Memory: 55.3 MB (9.50%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& h) {
-        int n=h.size(),m=h[0].size();
-        vector<vector<int>> dis(n, vector<int>(m, INT_MAX));
-        dis[0][0]=0;
-        priority_queue<
-                        pair<int,pair<int,int>>,
-                        vector<pair<int,pair<int,int>>>,
-                        greater<pair<int,pair<int,int>>>
-                    > q;
-        q.push({0,{0,0}});
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size(), m=heights[0].size();
 
-        vector<int> r={0,-1,0,1};
-        vector<int> c={-1,0,1,0};
-        int ans=INT_MAX;
-        
-        while(!q.empty()){
-            int d=q.top().first;
-            int x=q.top().second.first;
-            int y=q.top().second.second;
-            q.pop();
+        priority_queue<pair<int,pair<int,int>>,
+                            vector<pair<int,pair<int,int>>>,
+                            greater<pair<int,pair<int,int>>>
+                        >pq;
 
-            if(x==n-1 && y==m-1)
-                return d;
-                
-            else{
-            for(int i=0;i<4;i++){
-                int nr=x+r[i], nc=y+c[i];
-                if(nr>=0 && nc>=0 && nr<n && nc<m){
-                    int newd=max(d,abs(h[x][y]-h[nr][nc]));
-                    if(newd<dis[nr][nc]){
-                        q.push({newd,{nr,nc}});
-                        dis[nr][nc]=newd;
+        vector<vector<int>>dis(n,vector<int>(m,1e9));
+
+        pq.push({0,{0,0}});
+        vector<vector<int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!pq.empty()){
+            auto [e,pos]=pq.top();
+            auto [i,j]=pos;
+            pq.pop();
+
+            if(i==n-1 && j==m-1)
+                return e;
+
+            for(auto it:dir){
+                int ni=i+it[0], nj=j+it[1];
+                if(ni>=0 && ni<n && nj>=0 && nj<m){
+                    int eff=abs(heights[i][j]-heights[ni][nj]);
+                    int neff=max(eff,e);
+                    if(neff<dis[ni][nj]){
+                        pq.push({max(e,eff),{ni,nj}});
+                        dis[ni][nj]=neff;
                     }
                 }
-                
             }
-            } 
+
         }
         return -1;
     }
