afd06d3cd103974b96e632004515a00d5e640095
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-12 19:43:43 +0530

    Time: 12 ms (13.71%) | Memory: 21.6 MB (7.63%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,55 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>> adj(n);
+        vector<vector<vector<int>>>adj(n);
         for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-
-        queue<pair<int,pair<int,int>>> q;
-        q.push({-1,{src,0}});
-        vector<int> dis(n,INT_MAX);
-        dis[src]=0;
-
+        
+        vector<int>p(n,1e9);
+        // p[0]=0;
+        queue<tuple<int,int,int>>q;
+        q.push({0,src,-1});          
         while(!q.empty()){
-            int stop=q.front().first;
-            int node=q.front().second.first;
-            int cost=q.front().second.second;
+            auto[price,node,stop]=q.front();
             q.pop();
-            if((stop == k && node==dst) || (stop==k && node!=dst))
-                continue;
+
+            if(stop==k) continue;
+
             for(auto it:adj[node]){
-                if(dis[it.first]>cost+it.second){
-                    dis[it.first]=cost+it.second;
-                    q.push({stop+1,{it.first,dis[it.first]}});
+                if(p[it[0]]>price+it[1]){
+                    q.push({price+it[1],it[0],stop+1});
+                    p[it[0]]=price+it[1];
                 }
             }
         }
-        return dis[dst]==INT_MAX?-1:dis[dst];
-    }
+        return p[dst]==1e9?-1:p[dst];
+    }   
+    // int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    //     vector<vector<pair<int,int>>> adj(n);
+    //     for(auto it:flights){
+    //         adj[it[0]].push_back({it[1],it[2]});
+    //     }
+
+    //     queue<pair<int,pair<int,int>>> q;
+    //     q.push({-1,{src,0}});
+    //     vector<int> dis(n,INT_MAX);
+    //     dis[src]=0;
+
+    //     while(!q.empty()){
+    //         int stop=q.front().first;
+    //         int node=q.front().second.first;
+    //         int cost=q.front().second.second;
+    //         q.pop();
+    //         if((stop == k && node==dst) || (stop==k && node!=dst))
+    //             continue;
+    //         for(auto it:adj[node]){
+    //             if(dis[it.first]>cost+it.second){
+    //                 dis[it.first]=cost+it.second;
+    //                 q.push({stop+1,{it.first,dis[it.first]}});
+    //             }
+    //         }
+    //     }
+    //     return dis[dst]==INT_MAX?-1:dis[dst];
+    // }
 };
