e1328bebaf7c1ec93f6afb13f793ac8ee7d87b25
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-23 15:14:16 +0530

    Time: 99 ms (26.42%) | Memory: 36.1 MB (26.60%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,43 +1,69 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int n=heights.size(), m=heights[0].size();
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int m=h.size(),n=h[0].size();
 
-        priority_queue<pair<int,pair<int,int>>,
-                            vector<pair<int,pair<int,int>>>,
-                            greater<pair<int,pair<int,int>>>
-                        >pq;
-
-        vector<vector<int>>dis(n,vector<int>(m,1e9));
+        vector<vector<int>>dis(m,vector<int>(n,1e9));
         dis[0][0]=0;
+        queue<pair<int,int>>q;
+        q.push({0,0});
 
-        pq.push({0,{0,0}});
-        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
-        while(!pq.empty()){
-            auto [e,pos]=pq.top();
-            auto [i,j]=pos;
-            pq.pop();
+        int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};         // IMP FOR EFFICIENCT
+        while(!q.empty()){
+            auto [x,y]=q.front();
+            q.pop();
 
-            
-
-            if(i==n-1 && j==m-1)
-                return e;
-
-            if(e>dis[i][j]) 
-                continue;
-            for(auto &it:dir){
-                int ni=i+it[0], nj=j+it[1];
-                if(ni>=0 && ni<n && nj>=0 && nj<m){
-                    int eff=abs(heights[i][j]-heights[ni][nj]);
-                    int neff=max(eff,e);
-                    if(neff<dis[ni][nj]){
-                        pq.push({neff,{ni,nj}});
-                        dis[ni][nj]=neff;
+            for(auto it:dir){
+                int nx=x+it[0],ny=y+it[1];
+                if(nx>=0 && nx<m && ny>=0 && ny<n){
+                    int neff=max(dis[x][y],abs(h[x][y]-h[nx][ny]));
+                    if(dis[nx][ny]>neff){
+                        dis[nx][ny]=neff;
+                        q.push({nx,ny});
                     }
                 }
             }
-
         }
-        return -1;
+        return dis[m-1][n-1];
     }
+    // int minimumEffortPath(vector<vector<int>>& heights) {
+    //     int n=heights.size(), m=heights[0].size();
+
+    //     priority_queue<pair<int,pair<int,int>>,
+    //                         vector<pair<int,pair<int,int>>>,
+    //                         greater<pair<int,pair<int,int>>>
+    //                     >pq;
+
+    //     vector<vector<int>>dis(n,vector<int>(m,1e9));
+    //     dis[0][0]=0;         // IMP FOR EFFICIENCT
+
+    //     pq.push({0,{0,0}});
+    //     int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};         // IMP FOR EFFICIENCT
+    //     while(!pq.empty()){
+    //         auto [e,pos]=pq.top();
+    //         auto [i,j]=pos;
+    //         pq.pop();
+
+            
+
+    //         if(i==n-1 && j==m-1)
+    //             return e;
+
+    //         if(e>dis[i][j])         // IMP FOR EFFICIENCT
+    //             continue;
+    //         for(auto &it:dir){         // IMP FOR EFFICIENCT
+    //             int ni=i+it[0], nj=j+it[1];
+    //             if(ni>=0 && ni<n && nj>=0 && nj<m){
+    //                 int eff=abs(heights[i][j]-heights[ni][nj]);
+    //                 int neff=max(eff,e);
+    //                 if(neff<dis[ni][nj]){
+    //                     pq.push({neff,{ni,nj}});
+    //                     dis[ni][nj]=neff;
+    //                 }
+    //             }
+    //         }
+
+    //     }
+    //     return -1;
+    // }
 };
