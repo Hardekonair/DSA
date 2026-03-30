c168c4ba87f62466406c8349e6ceac502f675f8e
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-03-30 12:11:29 +0530

    Time: 2061 ms (5.02%) | Memory: 42.9 MB (34.40%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,58 +1,133 @@
 class Solution {
 public:
+    class DSU{
+        public:
+            vector<int>parent,rank;
+            DSU(int n){
+                parent.resize(n);
+                rank.resize(n,0);
 
-    int find(int i,vector<int>& par){
-        if(par[i]==i)
-            return i;
-        return par[i]=find(par[i],par);
-    }
+                for(int i=0;i<n;i++){
+                    parent[i]=i;
+                }
+            }
 
-    void uni(int i,int j,vector<int>& par){
-        int pari=find(i,par);
-        int parj=find(j,par);
-        if(pari==parj)  
-            return;
-        par[pari]=parj;
-    }
+            int find(int x){
+                if(parent[x]==x)    return x;
+                return parent[x]=find(parent[x]);
+            }
 
+            void uni(int a,int b){
+                int para=find(a),parb=find(b);
+                if(para==parb)  return;
+                if(rank[para]>rank[parb])
+                    parent[parb]=para;                
+                else if(rank[para]<rank[parb])
+                    parent[para]=parb;
+                else
+                    parent[parb]=para;
+                    rank[para]++;
+            }
+    };
     vector<vector<string>> accountsMerge(vector<vector<string>>& acc) {
         int n=acc.size();
-        unordered_map<string,int> mpp;
 
-        vector<int>par(n);
+        DSU dsu(n);
+
+        unordered_map<int,set<string>>mpp;
         for(int i=0;i<n;i++){
-            par[i]=i;
+            for(int j=1;j<acc[i].size();j++){
+                mpp[i].insert(acc[i][j]);
+            }
         }
 
         for(int i=0;i<n;i++){
-            for(int j=1;j<acc[i].size();j++){
-                if(mpp.find(acc[i][j])==mpp.end()){
-                    mpp[acc[i][j]]=i;
-                }
-                else{
-                    uni(i,mpp[acc[i][j]], par);
+            for(int j=i+1;j<n;j++){
+                // bool f=false;
+                for(auto it:mpp[j]){
+                    if(mpp[i].count(it)){
+                        dsu.uni(i,j);
+                        // f=true;
+                        continue;
+                    }
                 }
             }
         }
-        vector<vector<string>> vec(n);
-        for(auto it:mpp){
-            int i=find(it.second,par);
-            vec[i].push_back(it.first);
+
+        for(int i=0;i<n;i++){
+            int p=dsu.find(i);
+            if(p==i)continue;
+            for(auto it:mpp[i]){
+                mpp[p].insert(it);
+            }
         }
         vector<vector<string>>ans;
         for(int i=0;i<n;i++){
-            if(vec[i].size()==0)
-                continue;
-            
-            sort(vec[i].begin(),vec[i].end());
             vector<string>temp;
-            temp.push_back(acc[i][0]);
-            for(auto it:vec[i]) 
-                temp.push_back(it);
-            
+            if(dsu.find(i)==i){
+                temp.push_back(acc[i][0]);
+                for(auto it:mpp[i])
+                    temp.push_back(it);
+                
+            }
+            if(!temp.empty())
             ans.push_back(temp);
         }
-
         return ans;
     }
+
+    // int find(int i,vector<int>& par){
+    //     if(par[i]==i)
+    //         return i;
+    //     return par[i]=find(par[i],par);
+    // }
+
+    // void uni(int i,int j,vector<int>& par){
+    //     int pari=find(i,par);
+    //     int parj=find(j,par);
+    //     if(pari==parj)  
+    //         return;
+    //     par[pari]=parj;
+    // }
+
+    // vector<vector<string>> accountsMerge(vector<vector<string>>& acc) {
+    //     int n=acc.size();
+    //     unordered_map<string,int> mpp;
+
+    //     vector<int>par(n);
+    //     for(int i=0;i<n;i++){
+    //         par[i]=i;
+    //     }
+
+    //     for(int i=0;i<n;i++){
+    //         for(int j=1;j<acc[i].size();j++){
+    //             if(mpp.find(acc[i][j])==mpp.end()){
+    //                 mpp[acc[i][j]]=i;
+    //             }
+    //             else{
+    //                 uni(i,mpp[acc[i][j]], par);
+    //             }
+    //         }
+    //     }
+    //     vector<vector<string>> vec(n);
+    //     for(auto it:mpp){
+    //         int i=find(it.second,par);
+    //         vec[i].push_back(it.first);
+    //     }
+    //     vector<vector<string>>ans;
+    //     for(int i=0;i<n;i++){
+    //         if(vec[i].size()==0)
+    //             continue;
+            
+    //         sort(vec[i].begin(),vec[i].end());
+    //         vector<string>temp;
+    //         temp.push_back(acc[i][0]);
+    //         for(auto it:vec[i]) 
+    //             temp.push_back(it);
+            
+    //         ans.push_back(temp);
+    //     }
+
+    //     return ans;
+    // }
 };
