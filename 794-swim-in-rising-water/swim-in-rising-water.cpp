c7d2e0b6e024d6928678ffc25e86a30f7b087e7c
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-04-03 17:10:52 +0530

    Time: 23 ms (18.42%) | Memory: 16.8 MB (13.57%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,45 +1,76 @@
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
-        // SINCE STARTING AND ENDING POINT IS FIXED WE USE
-        // SINGLE SOURCE SHORTEST PATH
-        // DJIKSTRA ALGORITHM
-        
-        int n=grid.size();
-
-        vector<vector<int>> res(n,vector<int>(n,1e9));
-
-        priority_queue<pair<int,pair<int,int>>,         // {time,i,j}
+        priority_queue<pair<int,pair<int,int>>,
                             vector<pair<int,pair<int,int>>>,
-                            greater<pair<int,pair<int,int>>>
-                           > pq;
+                            greater<pair<int,pair<int,int>>>>pq;
+
+        vector<vector<int>>vis(grid.size(),vector<int>(grid.size(),0));
 
         pq.push({grid[0][0],{0,0}});
-        res[0][0]=grid[0][0];
-        vector<vector<int>> d={{1,0},{0,1},{-1,0},{0,-1}};
+    
+        vector<vector<int>>dir={{1,0},{0,1},{-1,0},{0,-1}};
         while(!pq.empty()){
-            int t=pq.top().first;
-            int r=pq.top().second.first;
-            int c=pq.top().second.second;
+            auto[time,cord]=pq.top();
+            auto [x,y]=cord;
             pq.pop();
-            
-            if(r==n-1 && c==n-1)    
-                return t;
+            // vis[x][y]=1;
 
-            if(t>res[r][c])
-                continue;
+            if(x==grid.size()-1 && y==grid.size()-1)
+                return time;
 
-            for(int i=0;i<4;i++){
-                int nr=r+d[i][0], nc=c+d[i][1];
-                if(nr<n && nc<n && nr>=0 && nc>=0){
-                    int nt=max(grid[nr][nc],t);
-                    if(nt<res[nr][nc]){
-                        res[nr][nc]=nt;
-                        pq.push({nt,{nr,nc}});
-                    }
+            for(auto it:dir){
+                int nx=x+it[0],ny=y+it[1];
+                if(nx>=0 && nx<grid.size() && ny>=0 && ny<grid.size() && vis[nx][ny]==0){
+                    vis[nx][ny]=1;
+                    int temp=max(time,grid[nx][ny]);
+                    pq.push({temp,{nx,ny}});
                 }
             }
+
         }
         return -1;
     }
+    // int swimInWater(vector<vector<int>>& grid) {
+    //     // SINCE STARTING AND ENDING POINT IS FIXED WE USE
+    //     // SINGLE SOURCE SHORTEST PATH
+    //     // DJIKSTRA ALGORITHM
+        
+    //     int n=grid.size();
+
+    //     vector<vector<int>> res(n,vector<int>(n,1e9));
+
+    //     priority_queue<pair<int,pair<int,int>>,         // {time,i,j}
+    //                         vector<pair<int,pair<int,int>>>,
+    //                         greater<pair<int,pair<int,int>>>
+    //                        > pq;
+
+    //     pq.push({grid[0][0],{0,0}});
+    //     res[0][0]=grid[0][0];
+    //     vector<vector<int>> d={{1,0},{0,1},{-1,0},{0,-1}};
+    //     while(!pq.empty()){
+    //         int t=pq.top().first;
+    //         int r=pq.top().second.first;
+    //         int c=pq.top().second.second;
+    //         pq.pop();
+            
+    //         if(r==n-1 && c==n-1)    
+    //             return t;
+
+    //         if(t>res[r][c])
+    //             continue;
+
+    //         for(int i=0;i<4;i++){
+    //             int nr=r+d[i][0], nc=c+d[i][1];
+    //             if(nr<n && nc<n && nr>=0 && nc>=0){
+    //                 int nt=max(grid[nr][nc],t);
+    //                 if(nt<res[nr][nc]){
+    //                     res[nr][nc]=nt;
+    //                     pq.push({nt,{nr,nc}});
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return -1;
+    // }
 };
