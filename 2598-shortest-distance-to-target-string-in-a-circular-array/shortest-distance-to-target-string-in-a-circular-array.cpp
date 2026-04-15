acdd60220fdaa975c8a8bfad08b052563f88afaa
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-04-15 14:30:49 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (37.22%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int closestTarget(vector<string>& words, string target, int s) {
-        int ans=-1;
         int n=words.size();
-        if(words[s]==target)   return 0;
-        int i=s,j=s;
-        int nxt=((i+1)%n);i++;
-        int prev=(j-1+n)%n;j--;
 
-        while(i-j<=n){
-            if(words[nxt]==target) return abs(i-s);
-            if(words[prev]==target) return abs(s-j);
-            nxt=(nxt+1)%n;i++;
-            prev=(prev-1+n)%n;j--;
+        if(words[s]==target)    return 0;
+
+        for(int steps=1;steps<n;steps++){
+            int left=(s-steps+n)%n;
+            int right=(s+steps)%n;
+
+            if(words[left]== target || words[right]==target)    return steps;
         }
         return -1;
     }
+    // int closestTarget(vector<string>& words, string target, int s) {
+    //     int ans=-1;
+    //     int n=words.size();
+    //     if(words[s]==target)   return 0;
+    //     int i=s,j=s;
+    //     int nxt=((i+1)%n);i++;
+    //     int prev=(j-1+n)%n;j--;
+
+    //     while(i-j<=n){
+    //         if(words[nxt]==target) return abs(i-s);
+    //         if(words[prev]==target) return abs(s-j);
+    //         nxt=(nxt+1)%n;i++;
+    //         prev=(prev-1+n)%n;j--;
+    //     }
+    //     return -1;
+    // }
 };
