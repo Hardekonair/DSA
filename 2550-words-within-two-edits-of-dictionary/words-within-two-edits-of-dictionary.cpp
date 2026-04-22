6e40bcac6c54e380267c41db905699a7f857c19e
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-04-22 12:36:45 +0530

    Time: 3 ms (74.87%) | Memory: 12.5 MB (70.59%) - LeetSync

--- 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
+++ 2550-words-within-two-edits-of-dictionary/words-within-two-edits-of-dictionary.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int diff(string a,string b){
+    int diff(string& a,string& b){        // it shoudl be bool but it still works
         int count=0;
         for(int i=0;i<a.size();i++){
             if(a[i]!=b[i])
@@ -11,16 +11,9 @@ public:
         return true;
     }
     vector<string> twoEditWords(vector<string>& q, vector<string>& d) {
-        // unordered_set<string>q;
-        // for(auto it:queries){
-        //     q.insert(it);
-        // }
-        // for(auto it:dictionary){
-        //     d.insert(it);
-        // }
         vector<string>ans;
-        for(auto it1:q){
-            for(auto it2:d){
+        for(auto& it1:q){
+            for(auto& it2:d){
                 if( diff(it1,it2) ){
                     ans.push_back(it1);
                     break;
