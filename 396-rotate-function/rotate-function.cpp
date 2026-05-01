5b8a974a4513156b4f8c37d55d140dcdf9eb056e
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-05-01 10:05:45 +0530

    Time: 0 ms (100.00%) | Memory: 99.6 MB (51.69%) - LeetSync

--- 396-rotate-function/rotate-function.cpp
+++ 396-rotate-function/rotate-function.cpp
@@ -11,7 +11,7 @@ public:
             sum+=nums[i];
         }
         maxans=prevans;
-        for(int i=n-1;i>0;i--){
+        for(int i=n-1;i>=0;i--){
             int newans=prevans+sum-(n*nums[i]);
 
             maxans=max(maxans,newans);
