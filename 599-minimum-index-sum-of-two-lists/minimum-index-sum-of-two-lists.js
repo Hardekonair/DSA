de4541acb04210217f947907813096246422ed45
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-06-16 00:01:31 +0530

    Time: 7 ms (81.97%) | Memory: 61.9 MB (50.82%) - LeetSync

---
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    const map=new Map();
+    for(let i=0;i<list1.length;i++){
+        map.set(list1[i],i);
+    }
+
+    let minisum=Infinity, result =[];
+
+    for(let j=0;j<list2.length;j++){
+        if(map.has(list2[j])){
+            const sum=map.get(list2[j])+j;
+            if(sum<minisum){
+                minisum=sum;
+                result=[(list2[j])];
+            }
+            else if(sum===minisum){
+                result.push(list2[j]);
+            }
+        }
+    }
+    return result;
+};
