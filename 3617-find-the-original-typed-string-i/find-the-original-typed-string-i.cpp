0de7fff227faf339984035137d3f3c14bbda3467
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-06-17 22:39:26 +0530

    Time: 3 ms (36.42%) | Memory: 8.8 MB (59.11%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int n=word.size();
+        char last=' ';
+        int temp=0;
+        for(auto it:word){
+            if(last!=it){
+                last=it;
+            }
+            else if(last==it)
+                temp++;
+        }
+        return temp+1;
+    }
+};
