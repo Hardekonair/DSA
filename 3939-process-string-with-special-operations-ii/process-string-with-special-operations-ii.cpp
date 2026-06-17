c174b0427a0c0c942037920cadbb6fb64fe2d275
Hardekonair <140601525+Hardekonair@users.noreply.github.com>
2026-06-17 19:15:21 +0530

    Time: 20 ms (89.66%) | Memory: 24 MB (67.82%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len=0;
+        for(auto it:s){
+            if(isalpha(it))
+                len++;
+            else if(len>0 && it=='*')
+                len--;
+            else if(it=='#')
+                len=len+len;
+            else if(it=='%')    
+                continue;
+            
+        }
+        if(k>=len)
+            return '.';
+
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='*')
+                len++;
+            else if(s[i]=='%')
+                k=len-k-1;
+            else if(s[i]=='#'){
+                len=len/2;
+                k=k>=len?k-len:k;
+            }
+            else
+                len--;
+            
+            if(len==k)  
+                return s[i];
+        }
+        return '.';
+    }
+};
